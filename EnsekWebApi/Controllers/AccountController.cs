f18f7ebe99d0c4e107f2b61228d8d9d45f7056e5
DARKKNIGHT\Nahmaan <nahmaan@gmail.com>
2022-01-14 16:32:15 +0000

    View running

---
+++ EnsekWebApi/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EnsekWebApi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public HttpConfiguration Configuration { get; private set; }
+
+        public AccountController()
+            : this(GlobalConfiguration.Configuration)
+        {
+        }
+
+        public AccountController(HttpConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
